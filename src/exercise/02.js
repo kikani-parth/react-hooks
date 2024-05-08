98cd06addaaa0df22813f30fe93fe23d31f7302b
Parth Kikani <parthkikani02@gmail.com>
2024-05-08 10:38:53 +0300

    complete 02.js exercise

--- src/exercise/02.js
+++ src/exercise/02.js
@@ -1,33 +1,61 @@
 // useEffect: persistent state
 // http://localhost:3000/isolated/exercise/02.js
 
-import * as React from 'react'
+import * as React from "react";
 
-function Greeting({initialName = ''}) {
-  // 🐨 initialize the state to the value from localStorage
-  // 💰 window.localStorage.getItem('name') ?? initialName
-  const [name, setName] = React.useState(initialName)
+/* EXTRA CREDIT 3 */
+function useLocalStorageState(key, defaultValue) {
+  const [stateVar, setStateVar] = React.useState(
+    () => window.localStorage.getItem(key) || defaultValue
+  );
 
-  // 🐨 Here's where you'll use `React.useEffect`.
-  // The callback should set the `name` in localStorage.
-  // 💰 window.localStorage.setItem('name', name)
+  console.log(typeof stateVar);
+
+  React.useEffect(() => {
+    window.localStorage.setItem(key, stateVar);
+  }, [key, stateVar]);
+
+  return [stateVar, setStateVar];
+}
+
+function Greeting({ initialName = "" }) {
+  /* EXERCISE */
+
+  // Get name from local storage
+  // initialName = window.localStorage.getItem("name") || "";
+
+  // const [name, setName] = React.useState(initialName);
+
+  /* EXTRA CREDIT 1 */
+
+  // const [name, setName] = React.useState(
+  //   () => window.localStorage.getItem("name") || initialName
+  // );
+
+  // React.useEffect(() => {
+  //   window.localStorage.setItem("name", name);
+  // }, [name]);
+
+  /* EXTRA CREDIT 3 */
+  const [name, setName] = useLocalStorageState("name", 0);
 
   function handleChange(event) {
-    setName(event.target.value)
+    setName(event.target.value);
   }
+
   return (
     <div>
       <form>
         <label htmlFor="name">Name: </label>
         <input value={name} onChange={handleChange} id="name" />
       </form>
-      {name ? <strong>Hello {name}</strong> : 'Please type your name'}
+      {name ? <strong>Hello {name}</strong> : "Please type your name"}
     </div>
-  )
+  );
 }
 
 function App() {
-  return <Greeting />
+  return <Greeting />;
 }
 
-export default App
+export default App;
